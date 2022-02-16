36d5d067ddd49a8b08e115017e87785638677002
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-16 16:26:58 -0500

    added comment, ready to add chart next iteration

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,8 @@
 import { Container, Typography } from '@mui/material';
 import React from 'react';
 
+/* This is the component that will be responsible for displaying our data fetched from the api, will include
+    the chart display */
 const Display = ( { data } ) => {
 
 
